e04d425532a2261c6f4386e87df0a0e01adc8fdc
clarkzsd <zsd1996@hotmail.com>
2017-12-21 15:19:31 +0800

    feat: minor UI change

--- public/assets/assets/src/WebIndex/views/Staff/components/index.js
+++ public/assets/assets/src/WebIndex/views/Staff/components/index.js
@@ -11,6 +11,14 @@ class Staff extends Component {
                       员工管理
                     </div>
                 </div>
+                <div className="staffContent">
+                    <div className="contentTopPanel">
+                        <Button icon="plus" type="primary">添加员工</Button>
+                        <Button icon="plus" type="primary">批量导入员工</Button>
+                        <div className="searchBar"></div>
+                    </div>
+                    <div></div>
+                </div>
             </div>
         );
     }
