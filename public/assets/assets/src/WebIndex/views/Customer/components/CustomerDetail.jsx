da4b2685e69d24a58d5470c0826fd94b0ae2f9d9
shuxiaotai <760684689@qq.com>
2017-12-23 15:09:53 +0800

    客户详情

---
+++ public/assets/assets/src/WebIndex/views/Customer/components/CustomerDetail.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import '../css/index.scss';
+import { Slider } from 'antd';
+
+const userImg = 'assets/assets/public/img/logo.jpg';
+const marks = {
+    0: '新入库',
+    20: '初步沟通',
+    40: '立项分析',
+    60: '方案制定',
+    100: '合同签订',
+};
+class CustomerDetail extends Component {
+    formatter = (value) => {
+        return `${value}%`;
+    };
+    render() {
+        return(
+            <div>
+                <div>
+                    <img src={userImg} className="userImg" />
+                    <span>刘小哈</span>
+                </div>
+                <div>
+                    <Slider marks={marks} step={null} defaultValue={0} tipFormatter={this.formatter} />
+                </div>
+            </div>
+        );
+    }
+}
+
+export default CustomerDetail;
