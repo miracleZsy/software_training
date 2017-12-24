741575b2c0654edcfc089328f6e4ed2d89abe96d
zhouqianyu <285942677@qq.com>
2017-12-24 12:42:30 +0800

    fix exception

--- app/Http/Controllers/ShareController.php
+++ app/Http/Controllers/ShareController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Share;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Webmozart\Assert\Assert;
 
 class ShareController extends Controller
@@ -37,28 +38,42 @@ class ShareController extends Controller
 
     public function insert(Request $request)
     {
-        Assert::notEmpty($_POST['uuid_received'], 'which user to send can not be empty');
-        Assert::notEmpty($_POST['customer_id'], 'which customer to send can not be empty');
-        Assert::numeric($_POST['customer_id'], 'customer id should be int type');
-        $uuidReceived = $_POST['uuid_received'];
-        $customerId = $_POST['customer_id'];
-        $id = Share::insertNewShare($request->get('user')->uuid, $uuidReceived, $customerId);
-        if ($id) $this->json_die([
-            'code' => 200,
-            'msg' => 'success',
-            'data' => $id
-        ]);
-        else $this->json_die(['code' => 403, 'msg' => 'unauthorized']);
+        try {
+            Assert::notEmpty($_POST['uuid_received'], 'which user to send can not be empty');
+            Assert::notEmpty($_POST['customer_id'], 'which customer to send can not be empty');
+            Assert::numeric($_POST['customer_id'], 'customer id should be int type');
+            $uuidReceived = $_POST['uuid_received'];
+            $customerId = $_POST['customer_id'];
+            $id = Share::insertNewShare($request->get('user')->uuid, $uuidReceived, $customerId);
+            if ($id) $this->json_die([
+                'code' => 200,
+                'msg' => 'success',
+                'data' => $id
+            ]);
+            else $this->json_die(['code' => 403, 'msg' => 'unauthorized']);
+        }catch (\InvalidArgumentException $e) {
+            $this->json_die(['code' => 407, 'msg' => $e->getMessage()]);
+        } catch (\Exception $e) {
+            Log::error($e->getMessage());
+            $this->json_die(['code' => 500, 'msg' => 'unknown error']);
+        }
     }
 
     public function delete(Request $request)
     {
-        Assert::notEmpty($_POST['id'], 'which id can not be empty');
-        Assert::numeric($_POST['id'], 'id should be int type');
-        $id = $_POST['id'];
-        $uuid = $request->get('user')->uuid;
-        $share = Share::where('id', $id)->where('uuid_received', $uuid)->orWhere('uuid_send', $uuid);
-        if ($share->delete()) $this->json_die(['code' => 200, 'msg' => 'success']);
-        else $this->json_die(['code' => 500, 'msg' => 'unknown error']);
+        try {
+            Assert::notEmpty($_POST['id'], 'which id can not be empty');
+            Assert::numeric($_POST['id'], 'id should be int type');
+            $id = $_POST['id'];
+            $uuid = $request->get('user')->uuid;
+            $share = Share::where('id', $id)->where('uuid_received', $uuid)->orWhere('uuid_send', $uuid);
+            if ($share->delete()) $this->json_die(['code' => 200, 'msg' => 'success']);
+            else $this->json_die(['code' => 500, 'msg' => 'unknown error']);
+        } catch (\InvalidArgumentException $e) {
+            $this->json_die(['code' => 407, 'msg' => $e->getMessage()]);
+        } catch (\Exception $e) {
+            Log::error($e->getMessage());
+            $this->json_die(['code' => 500, 'msg' => 'unknown error']);
+        }
     }
 }
