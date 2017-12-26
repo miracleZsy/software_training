6dec578f464086d266e64452af3106e3eeba2787
shuxiaotai <760684689@qq.com>
2017-12-26 09:24:41 +0800

    销售管理

--- routes/web.php
+++ routes/web.php
@@ -20,19 +20,8 @@ Route::post('/customer/delete','CustomerController@delete');
 Route::post('/customer/list','CustomerController@list');
 Route::post('/customer/select','CustomerController@select');
 Route::post('/customer/count','CustomerController@amountCount');
-<<<<<<< HEAD
-<<<<<<< HEAD
-Route::post('/customer/changePhase','CustomerController@changePhase');
-Route::post('/customer/getPhaseLog','CustomerController@getPhaseLog');
-=======
-
-//sale部分
-Route::post('/sale/customerManage','SaleController@customerManage');
->>>>>>> f1fb3a51c321a27d07008bb18bd376177eb79b47
-=======
 
 //sale部分
 Route::post('/sale/customerManage','SaleController@customerManage');
 Route::post('/customer/changePhase','CustomerController@changePhase');
 Route::post('/customer/getPhaseLog','CustomerController@getPhaseLog');
->>>>>>> 5f513062b7c866ef728c9fd9e724178e31a7b27b
