a6bb8d636fe59db9e40204868c8406f5031f48ab
shuxiaotai <760684689@qq.com>
2017-12-29 11:12:28 +0800

    销售计划

--- public/assets/assets/src/WebIndex/views/Sale/components/SaleManageRightTopContainer.jsx
+++ public/assets/assets/src/WebIndex/views/Sale/components/SaleManageRightTopContainer.jsx
@@ -8,7 +8,7 @@ const timePlanTags = ['不限', '最近2天', '最近七天', '最近30天'];
 
 class SaleManageRightTopContainer extends Component {
     render() {
-        const { setSaleTimeType, saleTimeType, fetchSaleAnalyse, saleTab, fetchSalePlan, saleCurrentPage } = this.props;
+        const { setSaleTimeType, saleTimeType, fetchSaleAnalyse, saleTab, fetchSalePlan, saleCurrentPage, setSaleCurrentPage } = this.props;
         return(
             <div className="saleManageRightTop">
                 {/*<SaleTag summarize="员工" tagsFromServer={staffTags} />*/}
@@ -21,6 +21,7 @@ class SaleManageRightTopContainer extends Component {
                     fetchSalePlan={fetchSalePlan}
                     saleTab={saleTab}
                     saleCurrentPage={saleCurrentPage}
+                    setSaleCurrentPage={setSaleCurrentPage}
                 />
             </div>
         );
