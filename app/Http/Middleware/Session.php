576aa3a123499692ebda07a545a30aaf18df546c
zhouqianyu <285942677@qq.com>
2018-01-06 15:56:55 +0800

    token join authority

--- app/Http/Middleware/Session.php
+++ app/Http/Middleware/Session.php
@@ -21,6 +21,7 @@ class Session
             $user = new User();
             $user->username = $token->getClaim('username');
             $user->uuid = $token->getClaim('uuid');
+
             $request->attributes->set('user', $user);
             return $next($request);
         } else die(json_encode([
